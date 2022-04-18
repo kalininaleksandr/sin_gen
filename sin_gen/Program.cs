6f2e8c53f735f00aa46c250a026dcdce731f9740
kalininaleksandr <96625@mail.ru>
2022-04-18 22:05:58 +0500

    Добавьте файлы проекта.

---
+++ sin_gen/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace sin_gen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sin lin = new Sin();
+            lin.calcPoints();
+            lin.showPoints();
+        }
+    }
+}
---
+++ sin_gen/Sin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sin_gen
+{
+    class Sin
+    {
+        public int amp, numPoints;
+        double[,] points;
+        double freq;
+        public Sin()
+        {
+            Console.WriteLine("Введите значение амплитуды: ");
+            amp = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение частоты: ");
+            freq = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите количество рассчитываемых координат: ");
+            numPoints = Convert.ToInt32(Console.ReadLine());
+
+        }
+        public void calcPoints()
+        {
+            double j = 0.0;
+            points = new double[numPoints,2];
+            for (int i = 0; i < numPoints; i++)
+            {
+                points[i, 0] = Math.Round(j,2);
+                points[i, 1] = Math.Round(amp * Math.Sin(2 * Math.PI * freq * j), 2);
+                j += 0.1;
+            }
+        }
+        public void showPoints()
+        {
+            Console.WriteLine("x    #y  ");
+            for (int i = 0; i < numPoints; i++)
+            {
+                Console.WriteLine(points[i,0] + "  #" + "   " + points[i, 1]);
+            }
+        }
+
+    }
+}
